74937ff0d9d6b751b716effb293f5edd6a509bcf
giulia_moukouyou <giuliamoukouyou@gmail.com>
2025-05-22 12:28:55 +0200

    Obtained a working bcrypt hash function, tested with Postman on get and post methods.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+
 app.use(express.json());
 
 const PORT = 3000;
@@ -23,11 +25,22 @@ app.get('/users', (req, res) => {
 }); 
 
 // post users
-app.post('/users', (req, res)=> {
-    const user = {
-        name: req.body.name,
-        password: req.body.password
+app.post('/users', async (req, res)=> {
+    try {
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        console.log('Salt is: ', salt);
+        console.log('The complete hashed password is: ', hashedPassword);
+        const user = {
+            name: req.body.name,
+            password: hashedPassword
+        }
+        users.push(user);
+        res.status(201).send();
     }
-    users.push(user);
-    res.status(201).send();
+    catch(error){
+        res.status(500).send(error);
+        console.log('There was an error: ', error)
+    }
+    // hash(salt + 'password') // hashed password: same password block at the base, different salt on top
 })
